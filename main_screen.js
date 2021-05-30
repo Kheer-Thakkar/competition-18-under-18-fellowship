8761edf2771b502e425fbb645a4904d8741bb68c
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-05-30 23:34:24 +0530

    Add files via upload

---
+++ main_screen.js
@@ -0,0 +1,59 @@
+function iy (){
+    window.alert("Hi there! I am Cespie The Robo Puppy and also your friend robo. I can assist you and can be your Personal Assistant. The things I can do are: Open YouTube, Take a selfie, Do Calculations, Google Search, Open WhatsUp, Intro of my Owner, Show u animations, Play a game, Play Songs... etc:. During the Journey you will also meet my friends Lucy the Robotic Cat and Twitter the Twit-Twit bird. ");
+  }
+ 
+ function navigate_youtube(){
+   window.location="https://www.youtube.com/"
+ }
+ 
+ function google(){
+   window.location="https://www.google.com/"
+}
+
+ function cam(){
+    window.location="camera.html"
+ }
+function whatsup(){
+window.location="chatting_room.html"
+}
+
+function intro(){
+   window.location="intro.html" 
+}
+function animations(){
+   window.location="animations.html" 
+}
+function calculator(){
+   window.location="calculator.html" 
+}
+function play_game(){
+   window.location="game_login_page.html"  
+}
+function play_songs(){
+   window.location="https://gaana.com/"
+}
+function break_time(){
+   window.location="break_time.html"
+
+}
+function Robotic_Cat(){
+   window.location="robotic_cat.html" 
+}
+function S_vs_D(){
+   window.location="S_vs_D.html" 
+}
+function Universe(){
+   window.location="Universe.html" 
+}
+function twit_twit_bird(){
+   window.location="twit_twit_bird.html" 
+}
+function letters_shivering_dance(){
+   window.location="letters_shivering_dance.html"
+}
+function aliens_UFO(){
+   window.location="alien's_UFO.html"
+}
+function firework(){
+   window.location="firework.html"
+}
